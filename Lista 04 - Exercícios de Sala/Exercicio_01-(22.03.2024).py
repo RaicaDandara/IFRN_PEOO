612c582772dda5b204516af465aba5d55786d8d9
RaicaDandara <160647354+RaicaDandara@users.noreply.github.com>
2024-03-22 17:55:58 -0300

    Update Exercicio_01-(22.03.2024).py

--- Lista 04 - Exercícios de Sala/Exercicio_01-(22.03.2024).py
+++ Lista 04 - Exercícios de Sala/Exercicio_01-(22.03.2024).py
@@ -14,3 +14,21 @@ print("Digite sua matrícula: ")
 matri = input() 
 ano = int(matri[0:4])
 print("O aluno(a)", nome, "cuja matrícula é", matri, "ingressou no IFRN no ano de", ano)
+
+# ou 
+class Aluno:
+    def __init__(self): #construtor
+        self.nome = ""  #atributo
+        self.matricula = ""
+    def ano_ingresso(self): #método
+        return int(self.matricula[0.4])
+
+a = Aluno()
+a.nome = "Rafaela"
+a.matricula = "20231011110007"
+b = Aluno()
+b.nome = "Miguel"
+b.matricula = "20201011110007"
+
+print(a.nome, a.matricula)
+print(b.nome, b.matricula)
