aa2984a593fe1de57541dfc63c9ea2c3c4acb871
RaicaDandara <160647354+RaicaDandara@users.noreply.github.com>
2024-07-09 18:43:37 +0000

    Atualizações Aula 04

--- Aula 4 (09.07.2024) - Encadeamento/encadeamento.py
+++ Aula 4 (09.07.2024) - Encadeamento/encadeamento.py
@@ -2,16 +2,23 @@
 
 class Triangulo:                    # Entidade
     def __init__(self):             # Contrutor
-        self.__b = 0                  # Atributos
+        # self.b = 0                # Atributos
+        self.__b = 0                # Atributos Encapsulado
         self.__h = 0
-    def set_base(self, valor):
+    def set_base(self, valor):      # armazenar um valor, ates é feita a varredura/verificação
         if valor >= 0:
             self.__b = valor
-    def get_base(self):
+        else:
+            raise ValueError()
+        # ou raise ValueError("Valor da base não pode ser nagativo") - raise ValueError pode conter/passar uma mensagem
+    def get_base(self):             # retornar um valor
         return self.__b
     def set_altura(self, valor):
         if valor >= 0:
             self.__h = valor
+        else:
+            raise ValueError()
+            # ou raise ValueError("Valor da altura não pode ser nagativo") - raise ValueError pode conter/passar uma mensagem
     def get_altura(self):
         return self.__h
     def calc_area(self):            # métodos = operação -> método de instância
@@ -21,10 +28,14 @@ class Triangulo:                    # Entidade
 class UI:                           # Interface com o usuário
     @staticmethod                   # prints e inputs nessa classe
     def main():                     # operação principal da UI -> método de cla
+        # Quando a classe segue de () ele chama o init
         x = Triangulo()
-        # Sem encapsulamento
+        # Sem encapsulamento - acesso direto ao atributo
+        # qualquer valor é armazenado
         # x.__b = float(input("Informe o valor da base: "))
         # x.__h = float(input("Informe o valor da altura: "))
+
+        # Com encapsulamento - acesso indireto ao atributo
         x.set_base = (float(input("Informe o valor da base: ")))
         x.set_altura = (float(input("Informe o valor da altura: ")))
         print(f"A base do triângulo é {x.get_base()}")
