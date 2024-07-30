901b67ae72f1c9dcad27485e218b03170554dae2
RaicaDandara <160647354+RaicaDandara@users.noreply.github.com>
2024-07-30 18:15:07 +0000

    Correção da questão 1 da lista

--- Lista 04 - Listas e Classes/Exemplo1.py
+++ Lista 04 - Listas e Classes/Exemplo1.py
@@ -1,4 +1,4 @@
-
+import random
 class Bingo:
     def __init__(self) -> None:
         self.__numBolas = 10
@@ -9,17 +9,44 @@ class Bingo:
         else:
             raise ValueError("Número de bolas tem que ser positivo")
     def proximo(self):                    # Sorteia uma bola 
+        if len(self.__sorteados) == self.__numBolas:
+            return "Fim do Jogo"
         n = random.randint(1, self.__numBolas)
-        print("Dentro no método: ", n)
+        # print("Dentro no método: ", n)
         while n in self.__sorteados:
             n = random.randint(1, self.__numBolas)
+            # print("Dentro no método: ", n)
         self.__sorteados.append(n)
+        # print(self.__sorteados)
         return n
+    def sorteados(self):
+        return sorted(self.__sorteados)
 
+class UI:
+    @staticmethod
+    def menu():
+        print("1-Iniciar Jogo, 2-Próximo, 3-Sorteados, 4-Fim")
+        return int(input("Escolha uma opção: "))
+    @staticmethod
+    def main():
+        b = Bingo()
+        print("Bem-vindo(a) ao IF Bingo")
+        op = 0
+        while op != 4:
+            op = UI.menu()
+            if op == 1: UI.iniciar_jogo(b)
+            if op == 2: UI.proximo(b)
+            if op == 3: UI.sorteados(b)
+        print("Bye")
+    @staticmethod
+    def iniciar_jogo(b):
+        n = int(input("Informe o número de bolas: "))
+        b.iniciar(n)
+    @staticmethod
+    def proximo(b):
+        print(b.proximo())
+    @staticmethod
+    def sorteados(b):        
+        print(b.sorteados())
 
-b = Bingo()
-print(b.proximo())
-print(b.proximo())
-print(b.proximo())
-print(b.proximo())
-print(b.proximo())
+UI.main()
