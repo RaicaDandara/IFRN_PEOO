a67a4b1c1c839c5e3b8e91d67257f085ffe2c481
RaicaDandara <160647354+RaicaDandara@users.noreply.github.com>
2024-08-29 17:02:29 -0300

    Update Exemplo1.py

--- Lista 04 - Listas e Classes/Exemplo1.py
+++ Lista 04 - Listas e Classes/Exemplo1.py
@@ -1,57 +1,43 @@
-# Escrever a classe Bingo de acordo com o diagrama UML apresentado.
-# A classe é utilizada para realizar um jogo de bingo.
-# O método Iniciar inicia uma partida, definindo o número de bolas do jogo.
-# O método Próximo sorteia uma bola, retornando o seu número (deve ser um valor entre um e o número de bolas ou menos um, caso todas as bolas já tenham sido sorteadas).
-# O método Sorteados retorna uma lista com todas as bolas já sorteadas. Insira outros atributos e métodos nas classes, caso necessário.
-
-# Diagrama:
-# Bingo
-# ----------------
-# -numBolas: int
-# ----------------
-# + numBolas
-
 import random
 class Bingo:
-    def __init__(self) -> None:
+    def __init__(self):
         self.__numBolas = 10
         self.__sorteados = []
-    def iniciar(self, numBolas):          # set_num_bolas
-        if numBolas > 0:
-            self.__numBolas = numBolas
-        else:
-            raise ValueError("Número de bolas tem que ser positivo")
-    def proximo(self):                    # Sorteia uma bola 
-        if len(self.__sorteados) == self.__numBolas:
-            return "Fim do Jogo"
+    def iniciar(self, numBolas):  # set_num_bolas
+        if numBolas > 0: self.__numBolas = numBolas
+        else: raise ValueError("Número de bolas tem que ser positivo")
+        self.__sorteados.clear()
+    def proximo(self):            # sorteia uma bola
+        if len(self.__sorteados) == self.__numBolas: return "Fim do Jogo"
         n = random.randint(1, self.__numBolas)
-        # print("Dentro no método: ", n)
+        #print("Dentro no método:", n)
         while n in self.__sorteados:
             n = random.randint(1, self.__numBolas)
-            # print("Dentro no método: ", n)
+            #print("Dentro no método:", n)
         self.__sorteados.append(n)
-        # print(self.__sorteados)
+        #print(self.__sorteados)
         return n
     def sorteados(self):
         return sorted(self.__sorteados)
-    def __str__(self) -> str:
-        return f"Foram sorteadas {len(self.__sorteados)} do total de {self.__numBolas} bolar"
-
+    def __str__(self):
+        return f"Foram sorteadas {len(self.__sorteados)} do total de {self.__numBolas} bolas"
+    
 class UI:
     @staticmethod
     def menu():
-        print("1-Iniciar Jogo, 2-Próximo, 3-Sorteados, 4-Fim")
+        print("1-Iniciar Jogo, 2-Próximo, 3-Sorteados, 4-Info, 5-Fim")
         return int(input("Escolha uma opção: "))
     @staticmethod
     def main():
         b = Bingo()
         print("Bem-vindo(a) ao IF Bingo")
         op = 0
-        while op != 4:
+        while op != 5:
             op = UI.menu()
             if op == 1: UI.iniciar_jogo(b)
             if op == 2: UI.proximo(b)
             if op == 3: UI.sorteados(b)
+            if op == 4: UI.info(b)
         print("Bye")
     @staticmethod
     def iniciar_jogo(b):
@@ -63,6 +49,6 @@ class UI:
     @staticmethod
     def sorteados(b):        
         print(b.sorteados())
-    def
-
+    def info(b):        
+        print(b)
 UI.main()
