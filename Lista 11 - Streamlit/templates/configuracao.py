1c4400a75a08b44cc19d7fb873cf4ff11f1a2174
RaicaDandara <160647354+RaicaDandara@users.noreply.github.com>
2024-12-03 17:54:35 -0300

    Update configuracao.py

--- Lista 11 - Streamlit/templates/configuracao.py
+++ Lista 11 - Streamlit/templates/configuracao.py
@@ -47,6 +47,26 @@ class Configuracao:
                 st.success("Dados atualizados com sucesso!")
                 time.sleep(2)
                 st.rerun()
-
+        elif tipo == "Admin":
+            cliente_id = st.session_state.get("cliente_id")
+            if cliente_id is None:
+                st.error("Nenhum cliente logado.")
+                return
+    
+            cliente = next((c for c in View.cliente_listar() if c.id == cliente_id), None)
+    
+            if cliente:
+                nome = cliente.nome
+                email = cliente.email
+                fone = cliente.fone
+                senha = st.text_input("Informe a nova senha", cliente.senha, type="password")
+    
+                if st.button("Atualizar"):
+                    View.cliente_atualizar(cliente_id, nome, email, fone, senha)
+                    st.success("Dados atualizados com sucesso!")
+                    time.sleep(2)
+                    st.rerun()
+            else:
+                st.error("Cliente não encontrado.")
         else:
             st.error("Tipo de usuário não identificado.")
